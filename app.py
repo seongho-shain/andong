9aa9726898526b35f5d7d285bc31b907b0ea626a
seongho-shain <shain1912@gmail.com>
2024-11-02 10:38:33 +0900

    force to lowercase

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ def save_uploaded_file(uploaded_file):
     save_dir = "uploaded_files"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    file_path = os.path.join(save_dir, uploaded_file.name)
+     # 파일 확장자를 소문자로 변환
+    file_name, file_extension = os.path.splitext(uploaded_file.name)
+    file_extension = file_extension.lower()  # 확장자를 소문자로 변경
+    file_path = os.path.join(save_dir, f"{file_name}{file_extension}")
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     st.image(file_path)
